c4c2e4f6f9e2c35662e0cd94a260d4fecc10a9ea
sr1k3sh <sr1k3sh@gmail.com>
2022-01-19 19:44:18 +0545

    testing new

--- bechdey-front/src/App.js
+++ bechdey-front/src/App.js
@@ -5,7 +5,7 @@ import AppRoutes from './components/AppRoute';
 import { AuthProvider } from './context/Context';
 import Navbar from './components/Navbar';
 import 'popper.js';
-import './../node_modules/bootstrap/dist/js/bootstrap.esm';
+import './../node_modules/bootstrap/js/src/dropdown';
 function App() {
   return (
     <AuthProvider>
